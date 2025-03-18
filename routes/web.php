d4e99b18a30c3607439a3e703d8f1e59aa51f695
Falydra <daffa.khususedukasi@gmail.com>
2025-03-18 11:13:28 +0700

    fix: update route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -12,11 +13,24 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('Dashboard/Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::get('/dashboard/cart', function () {
+    return Inertia::render('Dashboard/Cart'); 
+})->middleware(['auth', 'verified'])->name('cart');
+
+Route::get('/dashboard/payment', function () {
+    return Inertia::render('Dashboard/Payment');
+})->middleware(['auth', 'verified'])->name('payment');
+
+Route::get('/dashboard/donors', function () {
+    return Inertia::render('Dashboard/Donors');
+})->middleware(['auth', 'verified'])->name('donors');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
