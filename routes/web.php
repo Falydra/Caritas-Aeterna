dc46c6632c009eaf2153e6219fc0905628018d98
Falydra <daffa.khususedukasi@gmail.com>
2025-03-23 13:47:20 +0700

    feat: added route each roles

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,29 @@
 <?php
 
+
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\SuperAdmin\DashboardController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\CartController;
 
 Route::get('/', function () {
+
+    $user = auth()->user();
+    $roleModel = $user? $user->roles()->withPivot('id')->first() : null;
+    $role = $roleModel? [
+        'id' => $roleModel->pivot->id,
+        'name' => $user,
+        'role' => $roleModel,
+    ] : null;
     return Inertia::render('Welcome', [
+        'auth' => [
+
+                'user' => $user,
+                'roles' => $role,
+        ],
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
@@ -31,6 +47,15 @@ Route::get('/dashboard/payment', function () {
 Route::get('/dashboard/donors', function () {
     return Inertia::render('Dashboard/Donors');
 })->middleware(['auth', 'verified'])->name('donors');
+
+Route::get('/dashboard/admin', [AdminDashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('admin.dashboard');
+
+Route::get('/dashboard/super-admin', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('super-admin.dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
