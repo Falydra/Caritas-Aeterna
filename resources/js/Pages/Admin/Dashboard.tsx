a93c78b938391a22be036fa39d1d7fe6a4540d9c
Falydra <daffa.khususedukasi@gmail.com>
2025-03-23 13:49:29 +0700

    feat: added dashboard admin and super-admin

---
+++ resources/js/Pages/Admin/Dashboard.tsx
@@ -0,0 +1,9 @@
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+
+export default function AdminDashboard() {
+    return (
+        <Authenticated>
+            
+        </Authenticated>
+    );
+}
---
+++ resources/js/Pages/SuperAdmin/Dashboard.tsx
@@ -0,0 +1,10 @@
+import Authenticated from "@/Layouts/AuthenticatedLayout";
+
+export default function SuperAdminDashboard() {
+    console.log("Super Admin Dashboard");
+    return (
+        <Authenticated>
+            
+        </Authenticated>
+    );
+}
