31bec92bb536b897f9eaa8af46955c77658861e9
Falydra <daffa.khususedukasi@gmail.com>
2025-03-23 13:48:12 +0700

    feat: added controller index page

---
+++ app/Http/Controllers/SuperAdmin/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Controllers\SuperAdmin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\User;
+
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+
+        $user = auth()->user();
+        $role = $user? $user->roles()->first() : null;
+        return Inertia::render('SuperAdmin/Dashboard', [
+            'user' => $user,
+            'roles' => $role
+        ] );
+    }
+}
