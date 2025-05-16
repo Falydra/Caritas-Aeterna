a53b2ef00db5e2988f263960b7991df030dc5f8c
Falydra <daffa.khususedukasi@gmail.com>
2025-05-16 21:36:03 +0700

    feat: added donation detail

---
+++ app/Http/Controllers/DonationDetailController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Donation;
+use Inertia\Inertia;
+
+
+class DonationDetailController extends Controller
+{
+    public function index($id )
+    {
+        $donation = Donation::with('initiator:id,username')->findOrFail($id);
+
+        return Inertia::render('Donation/DonationDetail', [
+            'donation' => $donation,
+        ]);
+    }
+}
