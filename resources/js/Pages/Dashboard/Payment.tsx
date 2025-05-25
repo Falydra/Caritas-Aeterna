5905cf63d39ba97947e38d4efd8558f3cfa92554
reyynaldi <reynaldi4536@gmail.com>
2025-05-25 17:56:56 +0700

    update: Updated logic for donate product in donation service

--- resources/js/Pages/Dashboard/Payment.tsx
+++ resources/js/Pages/Dashboard/Payment.tsx
@@ -15,7 +15,7 @@ export default function Payment() {
         {/* <div className="flex flex-col w-full h-full items-center justify-center"> */}
             <div id="payment" className="flex flex-col w-full items-start justify-end px-8 py-4">
                 <h1 className="text-primary-fg self-start flex text-2xl flex-row items-center justify-end font-semibold hover:text-primary-accent">
-                    Payment
+                    Payments
                 </h1>
             </div>
         {/* </div> */}
--- routes/web.php
+++ routes/web.php
@@ -122,11 +122,11 @@ Route::get('/donations/product/latest', [ProductDonationController::class, 'late
 Route::get('/donations/{donation}', [DonationController::class, 'show'])->name('donations.show');
 Route::get('/donations/search', [DonationController::class, 'search'])->name('donations.search');
 
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('books/create', [BookController::class, 'create'])->name('books.create');
-    Route::post('books', [BookController::class, 'store'])->name('books.store');
-});
+// Route::get('/books', [BookController::class, 'index'])->name('books.index');
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('books/create', [BookController::class, 'create'])->name('books.create');
+//     Route::post('books', [BookController::class, 'store'])->name('books.store');
+// });
 
 // donee application group
 Route::middleware(['auth', 'verified'])->group(function () {
