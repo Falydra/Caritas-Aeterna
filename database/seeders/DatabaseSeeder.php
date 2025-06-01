30e05197cef6901239d68cb819a80024cada3009
Qun Alfadrian <qunadrian@gmail.com>
2025-06-01 23:02:20 +0700

    checkpoint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,18 +12,9 @@ class DatabaseSeeder extends Seeder {
      * Seed the application's database.
      */
     public function run(): void {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
+        $this->call([
+            UserSeeder::class,
+        ]);
         User::query()->update(['email_verified_at' => now()]);
-        // $this->call([
-        //     UserSeeder::class,
-        //     // BookSeeder::class,
-        //     DonationSeeder::class,
-        // ]);
     }
 }
