43090ff9dbc0661ed14f5d7130852318681217fc
Qun Alfadrian <qunadrian@gmail.com>
2025-06-12 22:18:39 +0700

    checkpoint

--- app/Http/Controllers/Donee/DoneeDashboardController.php
+++ app/Http/Controllers/Donee/DoneeDashboardController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\Donee;
 
-use App\Http\Controllers\Controller;
-use App\Models\Donation;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use App\Models\Donee;
-
+use App\Models\Donation;
+use App\Models\Facility;
+use App\Models\BookDonation;
+use App\Models\DonationItem;
+use Illuminate\Http\Request;
+use App\Models\ProductDonation;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DoneeDashboardController extends Controller {
     public function index() {
@@ -61,24 +64,64 @@ class DoneeDashboardController extends Controller {
             ]);
         }
 
-        $items = $donation->select([
+        $donationData = $donation->select([
             'id',
             'initiator_id',
             'type',
             'type_attributes',
             'title'
-        ])->with([
-            'donorDonations:id,donation_id',
-            'donorDonations.funds:id,order_id,donor_donation_id,amount,status,updated_at',
-            'donorDonations.donationItem:id,donor_donation_id,product_amount,package_picture,resi,status,updated_at'
         ])->first();
+
+        if ($donation->type() === ProductDonation::class) {
+            $books = $this->getBooks($donation);
+            $facilities = $this->getFacilities($donation);
+
+            return Inertia::render('Donation/DonatedItemList', [
+                'auth' => [
+                    'user' => $user,
+                    'roles' => $user->roleName()
+                ],
+                'data' => [
+                    'donation' => $donationData,
+                    'books' => $books,
+                    'facilities' => $facilities
+                ]
+            ]);
+        }
 
         return Inertia::render('Donation/DonatedItemList', [
             'auth' => [
                 'user' => $user,
                 'roles' => $user->roleName()
             ],
-            'data' => $items
+            'data' => []
         ]);
+    }
+
+    protected function getBooks(ProductDonation $donation) {
+        $data = BookDonation::whereHas(
+            'donation',
+            function ($q) use ($donation) {
+                $q->where('donation_id', $donation->id);
+            })->with([
+                'book:isbn,title,authors,published_year',
+                'donationItem:id,donor_donation_id,product_amount,package_picture,status',
+                'donationItem.donorDonation:id,donor_id',
+                'donationItem.donorDonation.donor:id,username',
+            ])->get();
+
+        return $data;
+    }
+
+    protected function getFacilities(ProductDonation $donation) {
+        $data = Facility::whereHas(
+            'donation',
+            function ($q) use ($donation) {
+                $q->where('product_donation_id', $donation->id);
+            })->with([
+                'donationItem',
+            ])->get();
+
+        return $data;
     }
 }
--- resources/js/Pages/Donation/DonatedItemList.tsx
+++ resources/js/Pages/Donation/DonatedItemList.tsx
@@ -1,60 +1,370 @@
 import Authenticated from "@/Layouts/AuthenticatedLayout";
+import { PageProps, User } from "@/types";
 import { Inertia } from "@inertiajs/inertia";
-import { Button, capitalize, styled } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import { usePage } from "@inertiajs/react";
+import {
+    Button,
+    capitalize,
+    Chip,
+    createTheme,
+    Paper,
+    styled,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    ThemeProvider,
+    Typography,
+} from "@mui/material";
+import { amber, grey } from "@mui/material/colors";
 import { useState } from "react";
 
 const BackButton = styled(Button)(({ theme }) => ({
-    color: grey['A200'],
-    textTransform: 'capitalize'
+    color: grey["A200"],
+    textTransform: "capitalize",
 }));
 
-interface Fund {
-    id: number,
-    order_id: string
-    amount: string,
-    status: string,
-    updated_at: string
+declare module "@mui/material/styles" {
+    interface Palette {
+        edit: Palette["primary"];
+        light: Palette["primary"];
+    }
+
+    interface PaletteOptions {
+        edit?: PaletteOptions["primary"];
+        light?: PaletteOptions["primary"];
+    }
 }
 
-interface DonationItem {
-    id: number,
-    product_amount: number,
-    package_picture: string,
-    resi: string,
-    updated_at: string
+declare module "@mui/material/Chip" {
+    interface ChipPropsColorOverrides {
+        edit: true;
+        light: true;
+    }
+}
+
+
+const theme = createTheme({
+    palette: {
+        edit: {
+            main: amber[400],
+            light: amber[300],
+            dark: amber[500],
+            contrastText: grey[900],
+        },
+        light: {
+            main: "#ffffff",
+            light: "#ffffff",
+            dark: grey[200],
+            contrastText: grey[900],
+        },
+    },
+});
+
+
+interface Donation {
+    id: number;
+    initiator_id: number;
+    type: string;
+    title: string;
+}
+
+interface Donor {
+    id: number;
+    username: string;
 }
 
 interface DonorDonation {
-    id: number,
-    donation_id: number
+    id: number;
+    donor_id: number;
+    donor: Donor;
 }
 
-interface Data {
-    id: number,
-    type: string,
-    title: string
+interface BookPivot {
+    book_donation_id: number;
+    donation_item_id: number;
+    amount: number;
+    updated_at: string;
+}
+
+interface DonationItem {
+    id: number;
+    donor_donation_id: number;
+    product_amount: number;
+    package_picture: string;
+    status: string;
+    pivot: BookPivot;
+    donor_donation: DonorDonation;
+}
+
+interface Book {
+    isbn: string;
+    title: string;
+    authors: string[];
+    published_year: string;
+}
+
+interface BookDonation {
+    id: number;
+    donation_id: number;
+    isbn: string;
+    amount: number;
+    fulfilled_amount: number;
+    status: string;
+    book: Book;
+    donation_item: DonationItem[];
+}
+
+interface Facility {
+    id: number;
+    product_donation_id: number;
+    name: string;
+    description: string;
+    dimension: string;
+    material: string;
+    price: string;
+    status: string;
+    updated_at: string;
+    donation_item: DonationItem[];
+}
+
+interface ProductData {
+    donation: Donation;
+    books: BookDonation[];
+    facilities: Facility[];
+}
+
+interface FundraiserData {
+    donation: Donation;
+}
+
+interface DonatedItemProps extends PageProps {
+    auth: {
+        user: User;
+        roles: string;
+    };
+    data: ProductData | FundraiserData;
+}
+
+function formatDate(date: string) {
+    return new Date(date).toISOString().replace("T", " ").slice(0, 19);
+}
+
+function formatStatus(status: string) {
+    return status
+        .split("_")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
 }
 
 export default function DonatedItemList() {
+    const { auth, data } = usePage<DonatedItemProps>().props;
+
     const handleBack = () => {
-        Inertia.visit('/dashboard/donee');
+        Inertia.visit("/dashboard/donee");
     };
+
+    const productDonationHeader = [
+        "Nama Donatur",
+        "Nama Produk",
+        "Jumlah",
+        "Status",
+    ];
+    const fundraisingHeader = ["Nama Donatur", "Jumlah", "Status"];
+
+    console.log("Item Data", data);
 
     return (
         <Authenticated>
             <div className="flex flex-col px-8 py-8 gap-4 w-full">
                 <div>
-                    <BackButton startIcon={(
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                        </svg>
-                    )} LinkComponent="a" href="/dashboard/donee">Kembali</BackButton>
+                    <BackButton
+                        startIcon={
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="size-4"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    d="M15.75 19.5 8.25 12l7.5-7.5"
+                                />
+                            </svg>
+                        }
+                        LinkComponent="a"
+                        href="/dashboard/donee"
+                    >
+                        Kembali
+                    </BackButton>
                 </div>
                 <div className="flex flex-col px-4 gap-4 w-full">
                     <h2 className="text-primary-fg text-2xl font-bold self-start">
-                        Daftar Sumbangan
+                        Daftar Sumbangan {data.donation.title}
                     </h2>
+                    <TableContainer
+                        component={Paper}
+                        sx={{ padding: "0 1rem" }}
+                    >
+                        <Table>
+                            <TableHead>
+                                {data.donation.type ===
+                                    "App\\Models\\ProductDonation" ? (
+                                    <TableRow>
+                                        {/* <TableCell align="left">No</TableCell> */}
+                                        <TableCell align="left">
+                                            Tanggal
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            Kategori
+                                        </TableCell>
+                                        {productDonationHeader.map((value) => (
+                                            <TableCell align="left" key={value}>
+                                                {value}
+                                            </TableCell>
+                                        ))}
+                                        <TableCell align="center">
+                                            Aksi
+                                        </TableCell>
+                                    </TableRow>
+                                ) : (
+                                    <TableRow>
+                                        <TableCell align="left">No</TableCell>
+                                        <TableCell align="left">
+                                            Tanggal
+                                        </TableCell>
+                                        <TableCell align="left">
+                                            Kategori
+                                        </TableCell>
+                                        {fundraisingHeader.map((value) => (
+                                            <TableCell align="left" key={value}>
+                                                {value}
+                                            </TableCell>
+                                        ))}
+                                        <TableCell align="center">
+                                            Aksi
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableHead>
+                            <TableBody>
+                                {data.donation.type ===
+                                    "App\\Models\\ProductDonation" ? (
+                                    <>
+                                        <TableRow>
+                                            <TableCell colSpan={10}>
+                                                <Typography fontWeight="bold">
+                                                    Daftar Sumbangan Buku
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                        {(data as ProductData).books.map(
+                                            (book) => (
+                                                (book.donation_item.map((item) => {
+                                                    return <TableRow key={item.id}>
+                                                        <TableCell align="left">
+                                                            {formatDate(item.pivot.updated_at)}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            Produk
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            {item.donor_donation.donor.username}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            {book.book.title}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            {item.pivot.amount}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            <ThemeProvider theme={theme}>
+                                                                <Chip
+                                                                    label={formatStatus(
+                                                                        item.status
+                                                                    )}
+                                                                    sx={{
+                                                                        textTransform:
+                                                                            "capitalize",
+                                                                    }}
+                                                                    color={
+                                                                        item.status ===
+                                                                            "finished"
+                                                                            ? "success"
+                                                                            : "edit"
+                                                                    }
+                                                                ></Chip>
+                                                            </ThemeProvider>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                }))
+                                            )
+                                        )}
+                                        <TableRow>
+                                            <TableCell colSpan={10}>
+                                                <Typography fontWeight="bold">
+                                                    Daftar Sumbangan Fasilitas
+                                                </Typography>
+                                            </TableCell>
+                                        </TableRow>
+                                        {(data as ProductData).facilities.map(
+                                            (facility) => (
+                                                (facility.donation_item.map((item) => {
+                                                    return <TableRow key={item.id}>
+                                                        <TableCell align="left">
+                                                            {formatDate(item.pivot.updated_at)}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            Produk
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            {item.donor_donation.donor.username}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            {facility.name}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            {item.pivot.amount}
+                                                        </TableCell>
+                                                        <TableCell align="left">
+                                                            <ThemeProvider theme={theme}>
+                                                                <Chip
+                                                                    label={formatStatus(
+                                                                        item.status
+                                                                    )}
+                                                                    sx={{
+                                                                        textTransform:
+                                                                            "capitalize",
+                                                                    }}
+                                                                    color={
+                                                                        item.status ===
+                                                                            "finished"
+                                                                            ? "success"
+                                                                            : "edit"
+                                                                    }
+                                                                ></Chip>
+                                                            </ThemeProvider>
+                                                        </TableCell>
+                                                    </TableRow>
+                                                }))
+                                            )
+                                        )}
+                                    </>
+                                ) : (
+                                    <TableRow>
+                                        <TableCell>
+                                            Hello
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
                 </div>
             </div>
         </Authenticated>
